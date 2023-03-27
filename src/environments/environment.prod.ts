27d7df04055aec25b00a1bd110a514a0c3b48dbd
BartekStachowicz <bartoszstachowicz@gmail.com>
2023-03-27 23:41:16 +0200

    spectools

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,10 @@
 export const environment = {
   production: true,
   apiKeyGoogle: '',
+  apiURL: '',
+  apiPromoKey: 'promo',
+  apiOfferKey: 'offers',
+  apiCounterUniqueKey: 'counter/unique',
+  apiCounterAllKey: 'counter/all',
+  apiAuthLoginKey: 'auht/login',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,10 @@
 export const environment = {
   production: false,
   apiKeyGoogle: '',
+  apiURL: 'http://localhost:3000/api/',
+  apiPromoKey: 'promo',
+  apiOfferKey: 'offers',
+  apiCounterUniqueKey: 'counter/unique',
+  apiCounterAllKey: 'counter/all',
+  apiAuthLoginKey: 'auht/login',
 };
