929534929f22e146f5c4c4093d03cb23740f46d1
Bartek Stachowicz <39810948+BartekStachowicz@users.noreply.github.com>
2023-03-27 23:45:00 +0200

    Delete src/environments directory

--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  apiURL: '',
-  apiPromoKey: 'promo',
-  apiOfferKey: 'offers',
-  apiCounterUniqueKey: 'counter/unique',
-  apiCounterAllKey: 'counter/all',
-  apiAuthLoginKey: 'auht/login',
-};
--- src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-  apiURL: 'http://localhost:3000/api/',
-  apiPromoKey: 'promo',
-  apiOfferKey: 'offers',
-  apiCounterUniqueKey: 'counter/unique',
-  apiCounterAllKey: 'counter/all',
-  apiAuthLoginKey: 'auht/login',
-};
